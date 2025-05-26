3fe2039d431b00661961f6184c7ed5ee3a82d8b1
聪 <505810824@qq.com>
2025-05-26 19:17:30 +0800

    add google login

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,115 @@
+// import { OAuth2Client } from 'google-auth-library';
+// import { google } from "googleapis";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+// const GOOGLE_AUTHORIZATION_URL =
+//     'https://accounts.google.com/o/oauth2/v2/auth?' +
+//     new URLSearchParams({
+//         scope: 'https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/drive.file',
+//         prompt: 'consent',
+//         access_type: 'offline',
+//         response_type: 'code'
+//     });
+
+async function refreshAccessToken(token: any) {
+    try {
+        const url =
+            'https://oauth2.googleapis.com/token?' +
+            new URLSearchParams({
+                client_id: process.env.NEXT_PUBLIC_CLIENT_ID || '',
+                client_secret: process.env.NEXT_PUBLIC_CLIENT_SECRET || '',
+                grant_type: 'refresh_token',
+                refresh_token: token.refreshToken
+            })
+
+        const response = await fetch(url, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            method: 'POST'
+        })
+
+        const refreshedTokens = await response.json()
+
+        if (!response.ok) {
+            throw refreshedTokens
+        }
+
+        return {
+            ...token,
+            accessToken: refreshedTokens.access_token,
+            accessTokenExpires: Date.now() + refreshedTokens.expires_in * 1000,
+            refreshToken: refreshedTokens.refresh_token ?? token.refreshToken // Fall back to old refresh token
+        }
+    } catch (error) {
+        console.log(error)
+
+        return {
+            ...token,
+            error: 'RefreshAccessTokenError'
+        }
+    }
+}
+const authOptions = {
+    // Configure one or more authentication providers
+    providers: [
+        GoogleProvider({
+            clientId: process.env.NEXT_PUBLIC_CLIENT_ID || '',
+            clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET || '',
+            authorization: {
+                params: {
+                    scope: 'profile https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file email openid',
+                    prompt: 'consent',
+                    access_type: 'offline'
+                },
+                // url: GOOGLE_AUTHORIZATION_URL
+            }
+        })
+    ],
+    secret: process.env.NEXT_PUBLIC_SECRET || process.env.NEXTAUTH_SECRET || '',
+    // session: {
+    //     strategy: 'jwt'
+    // },
+    callbacks: {
+        async jwt({ token, user, account }: any) {
+            // Persist the OAuth ,access_token to the token right after signin
+            // console.log('account', account);
+
+            if (account && user) {
+                return {
+                    accessToken: account.access_token,
+                    accessTokenExpires: Date.now() + account.expires_in * 1000,
+                    refreshToken: account.refresh_token,
+                    user
+                }
+            }
+
+            // Return previous token if the access token has not expired yet
+            if (Date.now() < token.accessTokenExpires) {
+                return token
+            }
+            // console.log('token', token);
+
+            // Access token has expired, try to update it
+            return refreshAccessToken(token)
+        },
+        async signIn() {
+            return true;
+        }
+        ,
+        async session({ session, token, user }: any) {
+            // Send properties to the client, like an access_token from a provider.
+            session.accessToken = token.accessToken
+
+            // Send properties to the client, like an access_token from a provider.
+            session.accessToken = token.accessToken
+            session.refreshToken = token.refreshToken
+            session.user = token.user
+            return session
+        },
+    }
+}
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
+
