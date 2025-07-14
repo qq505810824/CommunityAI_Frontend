afe45afe8ecc9976d45f0e246af4564eb682c3c8
聪 <505810824@qq.com>
2025-07-14 16:48:38 +0800

    update profile

--- src/app/(commonLayout)/profile/ProfileView.tsx
+++ src/app/(commonLayout)/profile/ProfileView.tsx
@@ -1,4 +1,5 @@
 import BackView from '@/app/components/base/back/BackView';
+import FormDetailView from '@/app/components/common/Widget/form';
 import { AccountModel } from '@/models/Account';
 import { UserFormData } from '@/utils/formData';
 import { useEffect, useState } from 'react';
@@ -28,12 +29,13 @@ function ProfileEditView(props: ViewProps) {
             <div className="w-full flex flex-col justify-center items-center ">
                 <div className="w-full sm:max-w-7xl px-4 py-4 flex flex-col  h-full overflow-y-auto">
                     <BackView title="Back" name="" />
-                    {/* <FormDetailView
+                    <FormDetailView
                         className={'p-4 sm:p-6 border rounded-lg'}
                         formData={formData}
                         disabled={submitting}
                         onSubmit={submit}
-                    /> */}
+                        storageName="avatars"
+                    />
                 </div>
             </div>
         </>
