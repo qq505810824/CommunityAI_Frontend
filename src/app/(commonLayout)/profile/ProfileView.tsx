022e2460c50e9ddc3b4b0d9418d59397c976b838
聪 <505810824@qq.com>
2025-07-14 16:41:55 +0800

    update profile

--- src/app/(commonLayout)/profile/ProfileView.tsx
+++ src/app/(commonLayout)/profile/ProfileView.tsx
@@ -1,5 +1,4 @@
 import BackView from '@/app/components/base/back/BackView';
-import FormDetailView from '@/app/components/common/Widget/form';
 import { AccountModel } from '@/models/Account';
 import { UserFormData } from '@/utils/formData';
 import { useEffect, useState } from 'react';
@@ -29,12 +28,12 @@ function ProfileEditView(props: ViewProps) {
             <div className="w-full flex flex-col justify-center items-center ">
                 <div className="w-full sm:max-w-7xl px-4 py-4 flex flex-col  h-full overflow-y-auto">
                     <BackView title="Back" name="" />
-                    <FormDetailView
+                    {/* <FormDetailView
                         className={'p-4 sm:p-6 border rounded-lg'}
                         formData={formData}
                         disabled={submitting}
                         onSubmit={submit}
-                    />
+                    /> */}
                 </div>
             </div>
         </>
